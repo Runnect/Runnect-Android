04c021b4cb10b15c1fee46eaa6e0b4f99f1da5a8
unam <dnska6657@naver.com>
2022-12-29 02:41:57 +0900

    #3 [Feat] 목표 보상 페이지 구현 완료
    논의사항이 있습니다. 이슈 참고해주세요!

--- app/src/main/java/com/example/runnect/data/api/GetRewardService.kt
+++ app/src/main/java/com/example/runnect/data/api/GetRewardService.kt
@@ -5,7 +5,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface GetRewardService {
-    @GET("8430d64f-841d-40e6-a92e-55123aab5dd5")
+    @GET("d72d7cad-92e4-43ab-a5b8-0a3ea936f400")
     suspend fun getRewardList(
     ): Response<GetRewardDto>
 }
