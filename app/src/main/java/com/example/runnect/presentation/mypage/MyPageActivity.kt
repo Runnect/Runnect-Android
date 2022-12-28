8a8e09397df23cef4acd9c3a611860f8ce3be66f
unam <dnska6657@naver.com>
2022-12-29 03:55:21 +0900

    [UI] 마이페이지 작업중
    백업용 커밋

---
+++ app/src/main/java/com/example/runnect/presentation/mypage/MyPageActivity.kt
@@ -0,0 +1,13 @@
+package com.example.runnect.presentation.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.runnect.R
+import com.example.runnect.binding.BindingActivity
+import com.example.runnect.databinding.ActivityMyRewardBinding
+
+class MyPageActivity : BindingActivity<ActivityMyRewardBinding>(R.layout.activity_my_page) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+}}
---
+++ app/src/main/java/com/example/runnect/presentation/mypage/MyPageViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.runnect.presentation.mypage
+
+import androidx.lifecycle.ViewModel
+
+class MyPageViewModel : ViewModel() {
+}
