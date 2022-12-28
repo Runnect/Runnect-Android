5c180f40f6d804e991b7337c3326f370a00da6fb
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-12-28 14:19:30 +0900

    #1 [Setting] Timber 설정

--- app/src/main/java/com/example/runnect/application/ApplicationClass.kt
+++ app/src/main/java/com/example/runnect/application/ApplicationClass.kt
@@ -9,5 +9,8 @@ import timber.log.Timber
 class ApplicationClass : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
