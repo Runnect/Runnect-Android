845ab651eba0221d29ede576a0cc3bf24e5c564d
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-01-01 18:26:14 +0900

    #13 [Setting] MainActivity 및 BottomNavigation 추가

---
+++ app/src/main/java/com/runnect/runnect/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.runnect.runnect.presentation
+
+import android.os.Bundle
+import com.runnect.runnect.R
+import com.runnect.runnect.binding.BindingActivity
+import com.runnect.runnect.databinding.ActivityMainBinding
+
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
