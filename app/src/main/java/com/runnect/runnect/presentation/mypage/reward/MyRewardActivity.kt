686dc5bfb908fc0bd27585d08ee58332a1f0acc0
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-01-08 01:41:37 +0900

    #17 [Chore] Fragment -> Activity
    MainActivity 이하 Fragment의 하위에 위치할 뷰들을 Fragment에서 Activity로 변경

---
+++ app/src/main/java/com/runnect/runnect/presentation/mypage/reward/MyRewardActivity.kt
@@ -0,0 +1,21 @@
+package com.runnect.runnect.presentation.mypage.reward
+
+import android.os.Bundle
+import com.runnect.runnect.R
+import com.runnect.runnect.binding.BindingActivity
+import com.runnect.runnect.databinding.ActivityMyRewardBinding
+
+class MyRewardActivity : BindingActivity<ActivityMyRewardBinding>(R.layout.activity_my_reward) {
+
+//    private val myRewardAdapter = MyRewardAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+//        val recyclerviewReward = binding.rvMyPageRewardStamps
+//        recyclerviewReward.adapter = myRewardAdapter
+
+}
+
+
