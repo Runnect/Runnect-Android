1c64b757e77d0d16ecca4dfb8f216d26a70e677e
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-01-08 18:19:24 +0900

    #17 [Feat] 키보드 관련 확장함수 구현
    - 키보드 올리기
    - 포커스 해제 및 키보드 내리기

--- app/src/main/java/com/runnect/runnect/util/extension/ContextExt.kt
+++ app/src/main/java/com/runnect/runnect/util/extension/ContextExt.kt
@@ -1,7 +1,7 @@
 package com.runnect.runnect.util.extension
 
-import android.app.Activity
 import android.content.Context
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 
@@ -9,6 +9,14 @@ import android.widget.Toast
 //Context 클래스에 속한 인스턴스 객체 - this
 //함수 내부에서는 this 키워드로 수신 객체 멤버 사용
 
-fun Context.showToast(message:String){
-    Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+fun Context.showToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+//키보드 내리기(포커스 해제)
+fun Context.clearFocus(view: View) {
+    val imm: InputMethodManager =
+        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
+    view.clearFocus()
 }
