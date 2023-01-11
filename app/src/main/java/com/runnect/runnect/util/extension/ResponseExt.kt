ad56b849dd604da655f735a57090ebfa0f77fddd
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-01-11 22:41:40 +0900

    #20 [Feat] 마이페이지 확장함수 및 UiState 구현

--- app/src/main/java/com/runnect/runnect/util/extension/ResponseExt.kt
+++ app/src/main/java/com/runnect/runnect/util/extension/ResponseExt.kt
@@ -1,3 +1,10 @@
+package com.runnect.runnect.util.extension
+
+import com.runnect.runnect.data.dto.RecordInfoDTO
+import com.runnect.runnect.data.dto.UserUploadCourseDTO
+import com.runnect.runnect.data.dto.response.PublicCourse
+import com.runnect.runnect.data.dto.response.Record
+
 fun Record.toData(): RecordInfoDTO {
     return RecordInfoDTO(
         id = id,
@@ -9,4 +16,36 @@ fun Record.toData(): RecordInfoDTO {
         time = timeConvert(time),
         pace = paceConvert(pace)
     )
+}
+
+fun PublicCourse.toData(): UserUploadCourseDTO {
+    return UserUploadCourseDTO(
+        id = id,
+        title = title,
+        img = image,
+        departure = departure.region + ' ' + departure.city
+    )
+}
+
+private fun timeConvert(time: String): String {
+    var hms = mutableListOf<String>()
+    hms = time.split(":").toMutableList()
+    if (hms[0] == "00") {
+        hms[0] = "0"
+    }
+    return "${hms[0]}:${hms[1]}:${hms[2]}"
+}
+
+private fun paceConvert(p: String): String {
+    var pace = mutableListOf<String>()
+    pace = p.split(":").toMutableList()
+    return if (pace[0] == "00") {
+        pace.removeAt(0)
+        if (pace[0][0] == '0') {
+            pace[0] = pace[0][1].toString()
+        }
+        "${pace[0]}’${pace[1]}”"
+    } else {
+        "${pace[0]}’${pace[1]}”${pace[2]}”"
+    }
 }
