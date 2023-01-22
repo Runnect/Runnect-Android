1c498f77fc1e81defbff177a4ff5cff51a6977e4
unam <dnska6657@naver.com>
2023-01-23 02:57:24 +0900

    #29 [Fix] CourseMain, Storage (Activity -> Fragment)
    - 바텀 네비게이션 연결을 위해 코스 메인, 보관함 Activity -> Fragment로 수정함
    - 바텀네비게이션 연결 해놓음
    - 빌드 정상적으로 되는 거 확인함
    - 서버 연결이 안 돼있을 때 대체 화면도 만들어야 할 듯

--- app/src/main/java/com/runnect/runnect/presentation/MainActivity.kt
+++ app/src/main/java/com/runnect/runnect/presentation/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.fragment.app.replace
 import com.runnect.runnect.R
 import com.runnect.runnect.binding.BindingActivity
 import com.runnect.runnect.databinding.ActivityMainBinding
+import com.runnect.runnect.presentation.coursemain.CourseMainFragment
 import com.runnect.runnect.presentation.discover.DiscoverFragment
 import com.runnect.runnect.presentation.mypage.MyPageFragment
+import com.runnect.runnect.presentation.storage.StorageFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,7 +23,7 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         binding.vm = viewModel
         binding.lifecycleOwner = this
         addListener()
-        changeFragment(R.id.menu_main_my_page)
+        changeFragment(R.id.menu_main_drawing)
     }
 
     private fun addListener() {
@@ -33,12 +35,19 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
 
     private fun changeFragment(menuItemId: Int) {
         when (menuItemId) {
+            R.id.menu_main_drawing -> supportFragmentManager.commit {
+                replace<CourseMainFragment>(R.id.fl_main)
+            }
+            R.id.menu_main_storage -> supportFragmentManager.commit {
+                replace<StorageFragment>(R.id.fl_main)
+            }
             R.id.menu_main_discover -> supportFragmentManager.commit {
                 replace<DiscoverFragment>(R.id.fl_main)
             }
             R.id.menu_main_my_page -> supportFragmentManager.commit {
                 replace<MyPageFragment>(R.id.fl_main)
             }
+
             else -> IllegalArgumentException("${this::class.java.simpleName} Not found menu item id")
         }
     }
