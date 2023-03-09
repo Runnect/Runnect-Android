de933cc402641e27457167fb1c1e0e0c11a90822
unam <dnska6657@naver.com>
2023-03-09 16:55:36 +0900

    #56[UI] 내가 그린 코스 - 삭제 dialog 세팅
    아직 api나 디자인이 나오지 않아서 할 수 있는 게 별로 없는데 미리 세팅해두면 나중에 조금만 바꿔서 쓰면 되니까 UI만 샘플로 세팅을 해주었음

--- app/src/main/java/com/runnect/runnect/presentation/mydrawdetail/MyDrawDetailActivity.kt
+++ app/src/main/java/com/runnect/runnect/presentation/mydrawdetail/MyDrawDetailActivity.kt
@@ -1,17 +1,25 @@
 package com.runnect.runnect.presentation.mydrawdetail
 
+import android.app.AlertDialog
 import android.content.ContentValues
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.core.net.toUri
 import com.bumptech.glide.Glide
 import com.naver.maps.geometry.LatLng
 import com.runnect.runnect.R
 import com.runnect.runnect.data.api.KApiCourse
+import com.runnect.runnect.data.model.DrawToRunData
 import com.runnect.runnect.data.model.MyDrawToRunData
 import com.runnect.runnect.databinding.ActivityMyDrawDetailBinding
+import com.runnect.runnect.presentation.MainActivity
 import com.runnect.runnect.presentation.countdown.CountDownActivity
+import kotlinx.android.synthetic.main.custom_dialog_delete.view.*
+import kotlinx.android.synthetic.main.custom_dialog_make_course.view.*
 import timber.log.Timber
 
 class MyDrawDetailActivity :
@@ -30,9 +38,44 @@ class MyDrawDetailActivity :
 //        binding.model = viewModel
         binding.lifecycleOwner = this
         getMyDrawDetail()
+        backButton()
         observing()
         toCountDownButton()
+        deleteButton()
 
+    }
+
+    fun customDialog(view: View) {
+        val myLayout = layoutInflater.inflate(R.layout.custom_dialog_delete, null)
+
+        val build = AlertDialog.Builder(view.context).apply {
+            setView(myLayout)
+        }
+        val dialog = build.create()
+//        dialog.setCancelable(false) // 외부 영역 터치 금지
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) // 내가 짠 layout 외의 영역 투명 처리
+        dialog.show()
+
+        myLayout.btn_delete_yes.setOnClickListener {
+            dialog.dismiss()
+        }
+        myLayout.btn_delete_no.setOnClickListener {
+            dialog.dismiss()
+        }
+
+    }
+
+    private fun deleteButton(){
+        binding.imgBtnDelete.setOnClickListener {
+            customDialog(binding.root)
+        }
+    }
+
+    private fun backButton() { //png가 imgBtn으로 하면 잘리길래 어차피 임시로 해놓는 거니까 imgView로 component를 추가해줬음
+        binding.imgBtnBack.setOnClickListener {
+            finish()
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+        }
     }
 
     override fun onBackPressed() {
