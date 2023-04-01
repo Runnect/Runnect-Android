21774fb5f78d619e5b1eaf52f94808097697c99c
unam <dnska6657@naver.com>
2023-04-02 03:30:04 +0900

    #64 [Fix] 내가그린코스-selection 오류 수정
    selection 라이브러리에 대한 이해가 떨어져서 적절한 대응을 못해왔던 거였음. 
    
    seleciton 추적을 인덱스를 기준으로 하니까 오류들이 많이 발생했는데 추적을 인덱스가 아닌 id값으로 해주었음. 이전에도 이렇게 한 번 시도는 해봤었는데 그때는 getItemId()의 return도 인덱스가 아닌 id값으로 바꿔줘야 한다는 걸 놓쳐서 해결을 잘 못했음.

--- app/src/main/java/com/runnect/runnect/presentation/storage/adapter/StorageMyDrawAdapter.kt
+++ app/src/main/java/com/runnect/runnect/presentation/storage/adapter/StorageMyDrawAdapter.kt
@@ -31,7 +31,7 @@ class StorageMyDrawAdapter(val myDrawClickListener: OnMyDrawClick) :
     }
 
     override fun getItemId(position: Int): Long {
-        return position.toLong()
+        return currentList[position].id.toLong()
     }
 
     inner class ItemViewHolder(val binding: ItemStorageMyDrawBinding) :
@@ -46,7 +46,7 @@ class StorageMyDrawAdapter(val myDrawClickListener: OnMyDrawClick) :
 
         fun bind(courseList: ResponseGetCourseDto.Data.Course) {
             binding.storageItem = courseList
-        } //여기는 dto를 구독해서 xml 단에서 바로 ui 바인딩을 할 수 있게끔 하려는 목적인듯
+        }
 
     }
 
@@ -64,46 +64,32 @@ class StorageMyDrawAdapter(val myDrawClickListener: OnMyDrawClick) :
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         holder.bind(currentList[position])
-//        holder.bindViews(currentList[position])
 
         with(holder) {
             binding.setVariable(BR.storageItem, getItem(position))
             binding.root.setOnClickListener {
                 myDrawClickListener.selectItem(currentList[position])
-                selectionTracker.select(position.toLong()) //이게 select을 실행시킴
+                selectionTracker.select(itemId) //이게 select을 실행시킴. 리사이클러뷰 아이템 고유 id 수집
             }
-            binding.selected = selectionTracker.isSelected(position.toLong())
-        }
-
-    }
-
-    //선택된 걸 별도의 list에다 저장하려고 애썼는데 그럴 필요가 없었음. 'selectionTracker.selection' 이 부분이 해당 역할을 이미 수행하고 있었음.
-    fun removeItem(selection: Selection<Long>) {
-
-        val itemList = mutableListOf<ResponseGetCourseDto.Data.Course>()
-        itemList.addAll(currentList)
-//        val selectedList = selection.toMutableList().map { it }
-        val selectedList = selection.map { it.toInt() }
-
-        Timber.tag(ContentValues.TAG)
-            .d("선택한 item 인덱스 값 : ${selectedList}")
-
-        Timber.tag(ContentValues.TAG)
-            .d("삭제 전 itemList id 값: ${itemList.map { it.id }}")
-//        // 인덱스를 내림차순으로 정렬
-//        selectedList.sortDescending()
-
-        for (i in selectedList) { //0,1,2
-            itemList.removeAt(i)
-            //보면 처음엔 [0]을 지우니까 717이 잘 삭제됐는데 이후 [1]을 삭제하려고 할 때 인덱스가 하나씩 앞으로 당겨지면서 716이 아닌 715가 삭제됨. 그래도 어떻게든 삭제는 해냈는데 [2] 때는 2개 남은
-            //상황에서 [2]라는 게 애초에 존재하지 않으니 IndexOutOfBoundsException이 떠서 앱이 죽어버림.
             Timber.tag(ContentValues.TAG)
-                .d("삭제 후: ${itemList.map { it.id }}")
+                .d("selection 값 : ${selectionTracker.selection}")
+            binding.selected = selectionTracker.isSelected(itemId)
         }
 
-
-        submitList(itemList) //인덱스 0만 하나 선택해서 지우면 인덱스가 안 밀리니까 반영이 잘 돼야 할 텐데 IndexOutOfBoundsException이 뜸. 이해가 안 되네.
     }
+    fun removeItem(selection: Selection<Long>) {
+        val itemList = currentList.toMutableList()
+        val selectedIds = selection.toMutableList()
+
+        for (id in selectedIds) {
+            val index = itemList.indexOfFirst { it.id.toLong() == id.toLong() }
+            if (index != -1) {
+                itemList.removeAt(index)
+            }
+        }
+        submitList(itemList)
+    }
+
 
 
     class Differ : DiffUtil.ItemCallback<ResponseGetCourseDto.Data.Course>() {
