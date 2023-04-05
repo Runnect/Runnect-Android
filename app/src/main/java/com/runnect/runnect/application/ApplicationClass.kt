4e4d76733d1295f474f62a4aab6b55aa70d84757
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-04-05 16:40:10 +0900

    #65 [Chore] PreferenceManager 추가

--- app/src/main/java/com/runnect/runnect/application/ApplicationClass.kt
+++ app/src/main/java/com/runnect/runnect/application/ApplicationClass.kt
@@ -1,16 +1,23 @@
 package com.runnect.runnect.application
 
 import android.app.Application
+import android.content.Context
 import com.runnect.runnect.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
 class ApplicationClass : Application() {
+
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+        appContext = applicationContext
+    }
+
+    companion object {
+        lateinit var appContext: Context
     }
 }
