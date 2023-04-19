8c85bb96bbb9d4cdb5dbaa274ba18822ffe089ad
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-04-19 23:44:33 +0900

    [Feat] 뷰페이저 어댑터 및 Indicator 적용 로직 구현

--- app/src/main/java/com/runnect/runnect/presentation/discover/DiscoverFragment.kt
+++ app/src/main/java/com/runnect/runnect/presentation/discover/DiscoverFragment.kt
@@ -4,35 +4,53 @@ import android.app.Activity.RESULT_OK
 import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.viewpager2.widget.ViewPager2
 import com.runnect.runnect.R
 import com.runnect.runnect.binding.BindingFragment
+import com.runnect.runnect.data.dto.DiscoverPromotionItemDTO
 import com.runnect.runnect.databinding.FragmentDiscoverBinding
 import com.runnect.runnect.presentation.detail.CourseDetailActivity
 import com.runnect.runnect.presentation.discover.adapter.CourseRecommendAdapter
+import com.runnect.runnect.presentation.discover.adapter.DiscoverPromotionAdapter
 import com.runnect.runnect.presentation.discover.load.DiscoverLoadActivity
 import com.runnect.runnect.presentation.discover.search.DiscoverSearchActivity
 import com.runnect.runnect.presentation.state.UiState
 import com.runnect.runnect.util.GridSpacingItemDecoration
-import com.runnect.runnect.util.callback.OnItemClick
 import com.runnect.runnect.util.callback.OnHeartClick
+import com.runnect.runnect.util.callback.OnItemClick
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
+import java.util.*
 
 @AndroidEntryPoint
 class DiscoverFragment : BindingFragment<FragmentDiscoverBinding>(R.layout.fragment_discover),
     OnItemClick, OnHeartClick {
     private val viewModel: DiscoverViewModel by viewModels()
-    private lateinit var adapter: CourseRecommendAdapter
+    private lateinit var courseRecommendAdapter: CourseRecommendAdapter
+    private lateinit var promotionAdapter: DiscoverPromotionAdapter
     private lateinit var startForResult: ActivityResultLauncher<Intent>
+    private lateinit var scrollHandler: Handler
+    private lateinit var timer: Timer
+    private lateinit var timerTask: TimerTask
+    private lateinit var scrollPageRunnable:Runnable
+    private var currentPosition = PAGE_NUM / 2
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val vpList = mutableListOf(
+            DiscoverPromotionItemDTO(R.drawable.discover_promotion3),
+            DiscoverPromotionItemDTO(R.drawable.discover_promotion3),
+            DiscoverPromotionItemDTO(R.drawable.discover_promotion3)
+        )
         binding.vm = viewModel
         binding.lifecycleOwner = this.viewLifecycleOwner
         initLayout()
@@ -40,6 +58,7 @@ class DiscoverFragment : BindingFragment<FragmentDiscoverBinding>(R.layout.fragm
         addListener()
         addObserver()
         setResultDetail()
+        setPromotion(binding.vpDiscoverPromotion,vpList)
     }
 
     private fun initLayout() {
@@ -78,7 +97,7 @@ class DiscoverFragment : BindingFragment<FragmentDiscoverBinding>(R.layout.fragm
                 UiState.Loading -> binding.indeterminateBar.isVisible = true
                 UiState.Success -> {
                     binding.indeterminateBar.isVisible = false
-                    initAdapter()
+                    setRecommendCourseAdapter()
                 }
                 UiState.Failure -> {
                     binding.indeterminateBar.isVisible = false
@@ -90,15 +109,56 @@ class DiscoverFragment : BindingFragment<FragmentDiscoverBinding>(R.layout.fragm
     }
 
 
-    private fun initAdapter() {
-        adapter = CourseRecommendAdapter(requireContext(), this, this).apply {
+    private fun setRecommendCourseAdapter() {
+        courseRecommendAdapter = CourseRecommendAdapter(requireContext(), this, this).apply {
             submitList(
                 viewModel.recommendCourseList
             )
         }
 
         binding.rvDiscoverRecommend.setHasFixedSize(true)
-        binding.rvDiscoverRecommend.adapter = adapter
+        binding.rvDiscoverRecommend.adapter = courseRecommendAdapter
+
+    }
+    private fun setPromotion(vp:ViewPager2,vpList:MutableList<DiscoverPromotionItemDTO>){
+        setPromotionAdapter(vpList)
+        setPromotionIndicator(vp)
+        setPromotionViewPager(vp)
+    }
+
+    private fun setPromotionAdapter(vpList: MutableList<DiscoverPromotionItemDTO>) {
+        promotionAdapter = DiscoverPromotionAdapter()
+        promotionAdapter.submitList(vpList)
+        binding.vpDiscoverPromotion.adapter = promotionAdapter
+        setPromotionIndicator(binding.vpDiscoverPromotion)
+    }
+    private fun setPromotionIndicator(vp: ViewPager2) {
+        val indicator = binding.ciDiscoverPromotion
+        indicator.setViewPager(vp)
+        indicator.createIndicators(FRAME_NUM, PAGE_NUM / 2)
+    }
+    private fun setPromotionViewPager(vp: ViewPager2) {
+        vp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        vp.setCurrentItem(PAGE_NUM / 2, false)
+        vp.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.ciDiscoverPromotion.animatePageSelected(position % FRAME_NUM)
+                currentPosition = position
+            }
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+                if (positionOffsetPixels == 0) {
+                    binding.ciDiscoverPromotion.animatePageSelected(position % FRAME_NUM)
+                }
+            }
+        }
+        )
+    }
     }
 
 
@@ -125,4 +185,7 @@ class DiscoverFragment : BindingFragment<FragmentDiscoverBinding>(R.layout.fragm
             R.anim.slide_out_left
         )
     }
+    companion object {
+        const val FRAME_NUM = 3
+        const val PAGE_NUM = 900
 }
