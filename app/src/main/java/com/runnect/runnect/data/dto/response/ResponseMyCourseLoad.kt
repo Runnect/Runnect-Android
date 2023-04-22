8c50d885624e113d3985d136711a20b591cf990e
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-04-22 12:23:21 +0900

    [Chore] Response 변경 machineId -> id
    헤더가 machineId에서 accessToken, refreshToken으로 바뀜에 따라 Response 데이터 id도 변경

--- app/src/main/java/com/runnect/runnect/data/dto/response/ResponseMyCourseLoad.kt
+++ app/src/main/java/com/runnect/runnect/data/dto/response/ResponseMyCourseLoad.kt
@@ -1,38 +1,56 @@
 package com.runnect.runnect.data.dto.response
+
+import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ResponseMyCourseLoad(
+    @SerializedName("data")
     val `data`: LoadData,
+    @SerializedName("message")
     val message: String,
+    @SerializedName("status")
     val status: Int,
+    @SerializedName("success")
     val success: Boolean
 )
 
 @Serializable
 data class LoadUser(
-    val machineId: String
+    @SerializedName("id")
+    val id: Int
 )
 
 @Serializable
 data class LoadDeparture(
+    @SerializedName("city")
     val city: String,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("region")
     val region: String,
+    @SerializedName("town")
     val town: String
 )
 
 @Serializable
 data class PrivateCourse(
+    @SerializedName("createdAt")
     val createdAt: String,
+    @SerializedName("departure")
     val departure: LoadDeparture,
+    @SerializedName("distance")
     val distance: Double,
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("image")
     val image: String
 )
 
 @Serializable
 data class LoadData(
+    @SerializedName("privateCourses")
     val privateCourses: List<PrivateCourse>,
+    @SerializedName("user")
     val user: LoadUser
 )
