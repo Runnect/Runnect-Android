07b4300e79cd0731c1dba354aac851bfb8c3d6a8
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-04-25 14:27:46 +0900

    [Chore] MyPageFragment resultLauncher 구현

--- app/src/main/java/com/runnect/runnect/presentation/mypage/MyPageFragment.kt
+++ app/src/main/java/com/runnect/runnect/presentation/mypage/MyPageFragment.kt
@@ -1,14 +1,18 @@
 package com.runnect.runnect.presentation.mypage
 
+import android.app.Activity.RESULT_OK
 import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import com.runnect.runnect.R
 import com.runnect.runnect.binding.BindingFragment
 import com.runnect.runnect.databinding.FragmentMyPageBinding
+import com.runnect.runnect.presentation.mypage.editname.MyPageEditNameActivity
 import com.runnect.runnect.presentation.mypage.history.MyHistoryActivity
 import com.runnect.runnect.presentation.mypage.reward.MyRewardActivity
 import com.runnect.runnect.presentation.mypage.upload.MyUploadActivity
@@ -20,6 +24,7 @@ import timber.log.Timber
 @AndroidEntryPoint
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
     private val viewModel: MyPageViewModel by activityViewModels()
+    private lateinit var resultEditNameLauncher: ActivityResultLauncher<Intent>
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.vm = viewModel
@@ -27,13 +32,30 @@ class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_
         viewModel.getUserInfo()
         addListener()
         addObserver()
+        setResultEditNameLauncher()
+    }
 
+    private fun setResultEditNameLauncher() {
+        resultEditNameLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val name = result.data?.getStringExtra(NICK_NAME) ?: viewModel.nickName.value
+                    viewModel.setNickName(name!!)
+                }
+            }
     }
 
     private fun addListener() {
         binding.ivMyPageEditFrame.setOnClickListener {
+            val intent = Intent(requireContext(), MyPageEditNameActivity::class.java)
+            intent.putExtra(NICK_NAME, "${viewModel.nickName.value}")
+            intent.putExtra(PROFILE, getProfileStamp(viewModel.stampId.value))
+            resultEditNameLauncher.launch(intent)
+            requireActivity().overridePendingTransition(
+                R.anim.slide_in_right,
+                R.anim.slide_out_left
+            )
         }
-
 
         binding.viewMyPageMainRewardFrame.setOnClickListener {
             startActivity(Intent(requireContext(), MyRewardActivity::class.java))
