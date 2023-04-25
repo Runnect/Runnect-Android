1693a64174131f51db898f21f3697fbc626ee2a2
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-04-25 14:35:26 +0900

    [Feat] 닉네임 수정 뷰 기능 구현
    - MyPageFragment로부터 받아온 nickname과 profileimg를 viewModel에 전달
    - 완료 버튼 클릭 시 닉네임 수정 통신
      -> 성공 시 setResult에 수정된 nickname 데이터를 담아 MyPageFragment로 이동

---
+++ app/src/main/java/com/runnect/runnect/presentation/mypage/editname/MyPageEditNameActivity.kt
@@ -0,0 +1,99 @@
+package com.runnect.runnect.presentation.mypage.editname
+
+import android.content.Intent
+import android.graphics.Rect
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import com.runnect.runnect.R
+import com.runnect.runnect.binding.BindingActivity
+import com.runnect.runnect.databinding.ActivityMyPageEditNameBinding
+import com.runnect.runnect.presentation.state.UiState
+import com.runnect.runnect.util.extension.clearFocus
+import com.runnect.runnect.util.extension.showToast
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MyPageEditNameActivity :
+    BindingActivity<ActivityMyPageEditNameBinding>(R.layout.activity_my_page_edit_name) {
+    private val viewModel: MyPageEditNameViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
+        initLayout()
+        addListener()
+        addObserver()
+    }
+
+    private fun initLayout() {
+        val nickName = intent.getStringExtra(NICK_NAME)
+        val profileImg = intent.getIntExtra(PROFILE, R.drawable.user_profile_basic)
+        viewModel.setNickName(nickName!!)
+        viewModel.setProfileImg(profileImg)
+    }
+
+    private fun addListener() {
+        binding.ivMyPageEditNameBack.setOnClickListener {
+            setResult(RESULT_CANCELED)
+            finish()
+        }
+        binding.tvMyPageEditNameFinish.setOnClickListener {
+            viewModel.updateNickName()
+        }
+        binding.etMyPageEditName.setOnEditorActionListener(object :
+            TextView.OnEditorActionListener {
+            override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    clearFocus(binding.etMyPageEditName)
+                    return true
+                }
+                return false
+            }
+        })
+    }
+
+    private fun addObserver() {
+        viewModel.uiState.observe(this) {
+            when (it) {
+                UiState.Empty -> binding.indeterminateBar.isVisible = false
+                UiState.Loading -> binding.indeterminateBar.isVisible = true
+                UiState.Success -> {
+                    binding.indeterminateBar.isVisible = false
+                    setResult(RESULT_OK, Intent().putExtra(NICK_NAME, viewModel.nickName.value))
+                    finish()
+                }
+                UiState.Failure -> {
+                    binding.indeterminateBar.isVisible = false
+                    }
+                }
+            }
+        }
+        }
+    }
+
+    //키보드 밖 터치 시, 키보드 내림
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        val focusView = currentFocus
+        if (focusView != null) {
+            val rect = Rect()
+            focusView.getGlobalVisibleRect(rect)
+            val x = ev!!.x.toInt()
+            val y = ev.y.toInt()
+            if (!rect.contains(x, y)) {
+                clearFocus(focusView)
+            }
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+
+    companion object {
+        const val NICK_NAME = "nickname"
+        const val PROFILE = "profile_img"
+    }
+}
