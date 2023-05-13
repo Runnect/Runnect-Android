c8e9bdca573f8503dba2271e547a1138b49a9d41
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-05-13 17:23:37 +0900

    [Refactor]

--- app/src/main/java/com/runnect/runnect/presentation/detail/CourseDetailViewModel.kt
+++ app/src/main/java/com/runnect/runnect/presentation/detail/CourseDetailViewModel.kt
@@ -7,12 +7,10 @@ import androidx.lifecycle.viewModelScope
 import com.runnect.runnect.R
 import com.runnect.runnect.data.dto.CourseDetailDTO
 import com.runnect.runnect.data.dto.request.RequestCourseScrap
-import com.runnect.runnect.data.model.DetailToRunData
 import com.runnect.runnect.domain.CourseRepository
 import com.runnect.runnect.presentation.state.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -35,29 +33,23 @@ class CourseDetailViewModel @Inject constructor(private val courseRepository: Co
         "1",
         listOf(listOf(1.1))
     )
+
     val courseDetail: CourseDetailDTO
         get() = _courseDetail
 
+    val errorMessage = MutableLiveData<String>()
 
     fun getCourseDetail(courseId: Int) {
         viewModelScope.launch {
             runCatching {
                 _courseDetailState.value = UiState.Loading
-                Timber.d("runCatching : 상세코스 UiState ${_courseDetailState.value}")
                 courseRepository.getCourseDetail(courseId)
             }.onSuccess {
                 _courseDetail = it
                 _courseDetailState.value = UiState.Success
-//                Timber.d("Success : 상세코스 값??? $it")
-                Timber.d("Success : 상세코스 _courseDetail값??? $_courseDetail")
-                Timber.d("Success : 상세코스 UiState ${_courseDetailState.value}")
             }.onFailure {
-                Timber.d(it.message)
-                Timber.d(it.cause)
-                Timber.d(it.localizedMessage)
                 _courseDetailState.value = UiState.Failure
-                Timber.d("Fail : 상세코스 UiState ${_courseDetailState.value}")
-                Timber.d("Fail 메세지 왜??? ${it.message}")
+                errorMessage.value = it.message
             }
         }
     }
@@ -67,9 +59,8 @@ class CourseDetailViewModel @Inject constructor(private val courseRepository: Co
             runCatching {
                 courseRepository.postCourseScrap(RequestCourseScrap(id, scrapTF.toString()))
             }.onSuccess {
-                Timber.d("onSuccess 메세지 : $it")
             }.onFailure {
-                Timber.d("onFailure 메세지 : $it")
+                errorMessage.value = it.message
             }
         }
     }
