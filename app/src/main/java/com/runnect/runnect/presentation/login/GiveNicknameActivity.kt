354303f0b39f79d03ba7db015db68ab65b99d093
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-05-20 15:05:58 +0900

    [Chore] 로그인 닉네임 입력 액티비티 생성

---
+++ app/src/main/java/com/runnect/runnect/presentation/login/GiveNicknameActivity.kt
@@ -0,0 +1,14 @@
+package com.runnect.runnect.presentation.login
+
+import android.os.Bundle
+import com.runnect.runnect.R
+import com.runnect.runnect.binding.BindingActivity
+import com.runnect.runnect.databinding.ActivityGiveNicknameBinding
+
+class GiveNicknameActivity :
+    BindingActivity<ActivityGiveNicknameBinding>(R.layout.activity_give_nickname) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
