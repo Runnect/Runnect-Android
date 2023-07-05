456ffb3a81484c2a5e6c5245eac0b78bef9b5d65
unam <dnska6657@naver.com>
2023-07-05 16:03:18 +0900

    #91 [fix] ServiceModule 생성

---
+++ app/src/main/java/com/runnect/runnect/di/ServiceModule.kt
@@ -0,0 +1,43 @@
+package com.runnect.runnect.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.runnect.runnect.BuildConfig
+import com.runnect.runnect.application.ApplicationClass
+import com.runnect.runnect.data.api.*
+import com.runnect.runnect.data.repository.*
+import com.runnect.runnect.data.source.remote.*
+import com.runnect.runnect.domain.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+    @Singleton
+    @Provides
+    fun provideKCourseService(@RetrofitModule.Runnect runnectRetrofit: Retrofit) = runnectRetrofit.create(KCourseService::class.java)
+
+    @Singleton
+    @Provides
+    fun providePCourseService(@RetrofitModule.Runnect runnectRetrofit: Retrofit) = runnectRetrofit.create(PCourseService::class.java)
+
+    @Singleton
+    @Provides
+    fun providePUserService(@RetrofitModule.Runnect runnectRetrofit: Retrofit) = runnectRetrofit.create(PUserService::class.java)
+
+    @Singleton
+    @Provides
+    fun provideKSearchService(@RetrofitModule.Tmap tmapRetrofit: Retrofit) = tmapRetrofit.create(KSearchService::class.java)
+
+}
