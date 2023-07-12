b8309b2a3067ae825fb88281f9b322e298f9c918
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-07-12 22:48:54 +0900

    [Chore] 불필요한 BindingAdapter 메소드 삭제

--- app/src/main/java/com/runnect/runnect/binding/BindingAdapter.kt
+++ app/src/main/java/com/runnect/runnect/binding/BindingAdapter.kt
@@ -1,20 +1,8 @@
 package com.runnect.runnect.binding
 
-import android.annotation.SuppressLint
-import android.graphics.Color
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import com.runnect.runnect.R
-
-@BindingAdapter("app:myDrawDetailImage")
-fun loadMyDrawDetailImage(view: ImageView, imageUrl: String) {
-    Glide
-        .with(view.context)
-        .load(imageUrl)
-        .circleCrop()
-        .into(view)
-}
 
 @BindingAdapter("app:storageImage")
 fun loadStorageImage(view: ImageView, imageUrl: String) {
@@ -32,23 +20,4 @@ fun loadEndRunImage(view: ImageView, imageUrl: String) {
         .load(imageUrl)
         .centerCrop()
         .into(view)
-}
-
-
-@BindingAdapter("app:image_select")
-fun ImageView.loadSelect(selected: Boolean) {
-    if (selected) {
-        setBackgroundResource(R.drawable.select)
-    } else {
-        setBackgroundColor(Color.parseColor("#FFFFFF"))
-    }
-}
-
-@BindingAdapter("app:image_check")
-fun ImageView.loadCheck(selected: Boolean) {
-    if (selected) {
-        setBackgroundResource(R.drawable.ic_select)
-    } else {
-        setBackgroundResource(R.drawable.ic_not_select)
-    }
 }
