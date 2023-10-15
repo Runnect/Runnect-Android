54b08482815da12e7785bf045c4c1df777b09797
leeeha <jxlhe46@seoultech.ac.kr>
2023-10-15 16:00:17 +0900

    [DEL] 불필요한 주석 삭제

--- app/src/main/java/com/runnect/runnect/util/extension/ContextExt.kt
+++ app/src/main/java/com/runnect/runnect/util/extension/ContextExt.kt
@@ -25,16 +25,10 @@ import kotlinx.android.synthetic.main.custom_dialog_delete.view.*
 import kotlinx.android.synthetic.main.custom_dialog_edit_mode.*
 import kotlinx.android.synthetic.main.fragment_bottom_sheet.btn_delete_yes
 
-
-//확장할 클래스 - Context 클래스
-//Context 클래스에 속한 인스턴스 객체 - this
-//함수 내부에서는 this 키워드로 수신 객체 멤버 사용
-
 fun Context.showToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
 
-//키보드 내리기(포커스 해제)
 fun Context.clearFocus(view: View) {
     val imm: InputMethodManager =
         getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
