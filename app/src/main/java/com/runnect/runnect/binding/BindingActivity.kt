eeb2bace26f8799c819cf84f1631cd7996dda2d7
leeeha <jxlhe46@seoultech.ac.kr>
2023-10-15 12:44:30 +0900

    [COMMENT] #231 주석 위치 변경

--- app/src/main/java/com/runnect/runnect/binding/BindingActivity.kt
+++ app/src/main/java/com/runnect/runnect/binding/BindingActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.viewbinding.ViewBinding
 
+/** 새로 Activity나 Fragment 만들면 xml이 data binding layout인지 확인할 것 */
 abstract class BindingActivity<B : ViewBinding>(@LayoutRes private val layoutRes: Int) :
     AppCompatActivity() {
     lateinit var binding: B
@@ -13,9 +14,5 @@ abstract class BindingActivity<B : ViewBinding>(@LayoutRes private val layoutRes
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, layoutRes)
-        //새로 Activity나 Fragment 만들면 xml이 data binding layout인지 확인할 것
-
     }
-
-
 }
