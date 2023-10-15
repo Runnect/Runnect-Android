eeb273c1901bc1f4487fc760d479046d3fa2f638
leeeha <jxlhe46@seoultech.ac.kr>
2023-10-15 11:41:41 +0900

    [CHORE] #231 Serializable 타입의 부가데이터를 얻는 함수의 이름 변경

--- app/src/main/java/com/runnect/runnect/util/extension/BundleExt.kt
+++ app/src/main/java/com/runnect/runnect/util/extension/BundleExt.kt
@@ -4,8 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import java.io.Serializable
 
-@Suppress("DEPRECATION")
-inline fun <reified T : Serializable> Bundle.customGetSerializable(key: String): T? {
+inline fun <reified T : Serializable> Bundle.getCompatibleSerializableExtra(key: String): T? {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         getSerializable(key, T::class.java)
     } else {
