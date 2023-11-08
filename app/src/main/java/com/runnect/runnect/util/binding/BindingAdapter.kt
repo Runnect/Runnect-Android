8d5e649bfa33b01b2a6a0752d9925fa918a60ac7
leeeha <jxlhe46@seoultech.ac.kr>
2023-11-08 16:07:02 +0900

    [REFACTOR] #267 바인딩 어댑터로 서버 이미지 보여주는 코드 통일시키기

--- app/src/main/java/com/runnect/runnect/util/binding/BindingAdapter.kt
+++ app/src/main/java/com/runnect/runnect/util/binding/BindingAdapter.kt
@@ -12,16 +12,10 @@ object BindingAdapter {
         imageView.load(resId)
     }
 
-    @BindingAdapter("setLocalImageByUri")
+    @BindingAdapter("setImageUrl")
     @JvmStatic
-    fun setLocalImageByUri(imageView: ImageView, uri: String) {
-        imageView.load(uri)
-    }
-
-    @BindingAdapter("setRemoteImageByUrl")
-    @JvmStatic
-    fun setRemoteImageByUrl(imageView: ImageView, url: String?) {
-        if (url == null) return
+    fun setImageUrl(imageView: ImageView, url: String?) {
+        if(url == null) return
         imageView.load(url)
     }
 
