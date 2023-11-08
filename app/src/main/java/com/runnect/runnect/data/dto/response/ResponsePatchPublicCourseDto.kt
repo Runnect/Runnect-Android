6d27148aa5756242e15086c48cf712c6a74801f6
leeeha <jxlhe46@seoultech.ac.kr>
2023-11-08 09:05:29 +0900

    [MOD] #267 BaseResponse 사용하도록 dto 변경

---
+++ app/src/main/java/com/runnect/runnect/data/dto/response/ResponsePatchPublicCourseDto.kt
@@ -0,0 +1,20 @@
+package com.runnect.runnect.data.dto.response
+
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponsePatchPublicCourseDto(
+    @SerializedName("publicCourse")
+    val publicCourse: PublicCourse
+)
+
+@Serializable
+data class PublicCourse(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("description")
+    val description: String
+)
