1856d8e1ae0c311b4e542d16d6de7b648b059e22
leeeha <jxlhe46@seoultech.ac.kr>
2023-12-09 23:25:15 +0900

    [DEL] #282 ResponseExt 파일에서 사용하지 않는 확장함수 삭제

--- app/src/main/java/com/runnect/runnect/util/extension/ResponseExt.kt
+++ app/src/main/java/com/runnect/runnect/util/extension/ResponseExt.kt
@@ -22,17 +22,6 @@ fun PublicCourseUpload.toData(): UserUploadCourseDTO {
     )
 }
 
-//fun SearchPublicCourse.toData(): CourseSearchDTO {
-//    return CourseSearchDTO(
-//        courseId = courseId,
-//        departure = departure.region + ' ' + departure.city,
-//        id = id,
-//        title = title,
-//        scrap = scrap,
-//        image = image
-//    )
-//}
-
 fun PrivateCourse.toData(): CourseLoadInfoDTO {
     return CourseLoadInfoDTO(
         id = id,
@@ -56,8 +45,7 @@ fun ResponsePostLogin.toData(): LoginDTO {
 }
 
 private fun timeConvert(time: String): String {
-    var hms = mutableListOf<String>()
-    hms = time.split(":").toMutableList()
+    val hms = time.split(":").toMutableList()
     if (hms[0] == "00") {
         hms[0] = "0"
     }
@@ -65,8 +53,7 @@ private fun timeConvert(time: String): String {
 }
 
 private fun paceConvert(p: String): String {
-    var pace = mutableListOf<String>()
-    pace = p.split(":").toMutableList()
+    val pace = p.split(":").toMutableList()
     return if (pace[0] == "00") {
         pace.removeAt(0)
         if (pace[0][0] == '0') {
