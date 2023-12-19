c1940c0ee471c585c9f10ea28be2100b29fb4dcc
leeeha <jxlhe46@seoultech.ac.kr>
2023-12-19 23:09:54 +0900

    [DEL] #297 사용하지 않는 상수 삭제

--- app/src/main/java/com/runnect/runnect/presentation/discover/DiscoverFragment.kt
+++ app/src/main/java/com/runnect/runnect/presentation/discover/DiscoverFragment.kt
@@ -437,8 +437,6 @@ class DiscoverFragment : BindingFragment<FragmentDiscoverBinding>(R.layout.fragm
     companion object {
         private const val BANNER_SCROLL_DELAY_TIME = 5000L
         private const val CENTER_POS_OF_INFINITE_BANNERS = Int.MAX_VALUE / 2
-        private const val PAGE_PRE_PATCH_DISTANCE = 1
-
         private const val EXTRA_PUBLIC_COURSE_ID = "publicCourseId"
         private const val EXTRA_ROOT_SCREEN = "rootScreen"
         const val EXTRA_EDITABLE_DISCOVER_COURSE = "editable_discover_course"
