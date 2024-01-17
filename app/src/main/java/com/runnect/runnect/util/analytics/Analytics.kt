7caeee042ca56d5bf7d94263e0e731c1fd58096e
HyeseonBaek <konkukbhs@gmail.com>
2024-01-17 20:40:39 +0900

    [FEAT] #315 GA 초기화 및 logEvent Util 구현

---
+++ app/src/main/java/com/runnect/runnect/util/analytics/Analytics.kt
@@ -0,0 +1,40 @@
+package com.runnect.runnect.util.analytics
+
+import android.content.Context
+import android.os.Bundle
+import com.google.android.gms.common.wrappers.InstantApps
+import com.google.firebase.analytics.FirebaseAnalytics
+
+object Analytics {
+    private const val STATUS_INSTALLED = "installed"
+    private const val STATUS_INSTANT = "instant"
+    private const val ANALYTICS_USER_PROP = "app_type"
+
+    private var firebaseAnalytics: FirebaseAnalytics? = null
+
+    fun initializeFirebaseAnalytics(context: Context) {
+        firebaseAnalytics = FirebaseAnalytics.getInstance(context)
+
+        // 현재 앱이 인스턴트 앱인지를 확인하고, 그 여부에 따라 UserProperty 다르게 세팅
+        if (InstantApps.isInstantApp(context)) {
+            setUserProperty(ANALYTICS_USER_PROP, STATUS_INSTANT)
+        } else {
+            setUserProperty(ANALYTICS_USER_PROP, STATUS_INSTALLED)
+        }
+    }
+
+    private fun setUserProperty(property: String, value: String) {
+        firebaseAnalytics?.setUserProperty(property, value)
+    }
+
+    fun logClickedItemEvent(itemName: String) {
+        val bundle = Bundle().apply {
+            putString(FirebaseAnalytics.Param.ITEM_NAME, itemName)
+        }
+        logEvent(FirebaseAnalytics.Event.SELECT_ITEM, bundle)
+    }
+
+    private fun logEvent(eventName: String, params: Bundle) {
+        firebaseAnalytics?.logEvent(eventName, params)
+    }
+}
