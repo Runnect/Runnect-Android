79867b84ebdb4d3ad35a9b605387663d547fe428
leeeha <jxlhe46@seoultech.ac.kr>
2024-01-24 00:41:49 +0900

    [FEAT] #307 정렬 버튼 클릭에 따라 텍스트 디자인 변경

--- app/src/main/java/com/runnect/runnect/presentation/discover/adapter/multiview/DiscoverMultiViewHolder.kt
+++ app/src/main/java/com/runnect/runnect/presentation/discover/adapter/multiview/DiscoverMultiViewHolder.kt
@@ -1,8 +1,12 @@
 package com.runnect.runnect.presentation.discover.adapter.multiview
 
+import android.content.Context
+import android.graphics.Typeface
+import android.widget.TextView
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.runnect.runnect.R
 import com.runnect.runnect.databinding.ItemDiscoverMultiviewMarathonBinding
 import com.runnect.runnect.databinding.ItemDiscoverMultiviewRecommendBinding
 import com.runnect.runnect.domain.entity.DiscoverMultiViewItem.MarathonCourse
@@ -13,6 +17,7 @@ import com.runnect.runnect.presentation.discover.model.EditableDiscoverCourse
 import com.runnect.runnect.util.custom.deco.DiscoverMarathonItemDecoration
 import com.runnect.runnect.util.custom.deco.DiscoverRecommendItemDecoration
 import com.runnect.runnect.util.custom.deco.GridSpacingItemDecoration
+import com.runnect.runnect.util.extension.colorOf
 import timber.log.Timber
 
 sealed class DiscoverMultiViewHolder(binding: ViewDataBinding) :
@@ -105,11 +110,41 @@ sealed class DiscoverMultiViewHolder(binding: ViewDataBinding) :
                 )
             }
         }
-
         private fun initSortButtonClickListener() {
             binding.tvDiscoverRecommendSortByDate.setOnClickListener {
-
+                val context = it.context ?: return@setOnClickListener
+                activateTextStyle(view = it as TextView, context = context)
+                deactivateOtherTextStyle(
+                    view = binding.tvDiscoverRecommendSortByScrap,
+                    context = context
+                )
+                onSortButtonClick.invoke(SORT_BY_DATE)
             }
+
+            binding.tvDiscoverRecommendSortByScrap.setOnClickListener {
+                val context = it.context ?: return@setOnClickListener
+                activateTextStyle(view = it as TextView, context = context)
+                deactivateOtherTextStyle(
+                    view = binding.tvDiscoverRecommendSortByDate,
+                    context = context
+                )
+                onSortButtonClick.invoke(SORT_BY_SCRAP)
+            }
+        }
+
+        private fun activateTextStyle(view: TextView, context: Context) {
+            view.setTextColor(context.colorOf(R.color.M1))
+            view.typeface = context.fontOf(R.font.pretendard_semibold, Typeface.NORMAL)
+        }
+
+        private fun deactivateOtherTextStyle(view: TextView, context: Context) {
+            view.setTextColor(context.colorOf(R.color.G2))
+            view.typeface = context.fontOf(R.font.pretendard_regular, Typeface.NORMAL)
+        }
+
+        companion object {
+            private const val SORT_BY_DATE = "date"
+            private const val SORT_BY_SCRAP = "scrap"
         }
     }
 }
