ffda301ab06dcbefc1e9b3e0ecad0635e8b29971
leeeha <jxlhe46@seoultech.ac.kr>
2024-01-28 23:10:55 +0900

    [FIX] #314 코스 검색 시 나오는 리스트에도 동일한 스크랩 로직 반영

--- app/src/main/java/com/runnect/runnect/presentation/discover/search/DiscoverSearchActivity.kt
+++ app/src/main/java/com/runnect/runnect/presentation/discover/search/DiscoverSearchActivity.kt
@@ -29,6 +29,7 @@ import com.runnect.runnect.util.extension.getCompatibleParcelableExtra
 import com.runnect.runnect.util.extension.hideKeyboard
 import com.runnect.runnect.util.extension.navigateToPreviousScreenWithAnimation
 import com.runnect.runnect.util.extension.showKeyboard
+import com.runnect.runnect.util.extension.showSnackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -141,6 +142,7 @@ class DiscoverSearchActivity :
 
     private fun addObserver() {
         setupCourseSearchStateObserver()
+        setupCourseScrapStateObserver()
     }
 
     private fun setupCourseSearchStateObserver() {
@@ -180,6 +182,26 @@ class DiscoverSearchActivity :
 
     private fun dismissProgressBar() {
         binding.pbDiscoverSearch.isVisible = false
+    }
+
+    private fun setupCourseScrapStateObserver() {
+        viewModel.courseScrapState.observe(this) { state ->
+            when (state) {
+                is UiStateV2.Success -> {
+                    val response = state.data ?: return@observe
+                    searchAdapter.updateCourseScrap(
+                        publicCourseId = response.publicCourseId.toInt(),
+                        scrap = response.scrapTF
+                    )
+                }
+
+                is UiStateV2.Failure -> {
+                    showSnackbar(binding.root, state.msg)
+                }
+
+                else -> {}
+            }
+        }
     }
 
     override fun dispatchTouchEvent(event: MotionEvent): Boolean {
