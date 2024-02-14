f4b19240641cfbaec7d64a06aa846f3e780189d1
leeeha <jxlhe46@seoultech.ac.kr>
2024-02-14 23:09:14 +0900

    [ADD] #319 머지하다가 빠뜨린 임포트문 추가

--- app/src/main/java/com/runnect/runnect/data/service/CourseService.kt
+++ app/src/main/java/com/runnect/runnect/data/service/CourseService.kt
@@ -9,6 +9,7 @@ import com.runnect.runnect.data.dto.response.ResponseGetCourseDetail
 import com.runnect.runnect.data.dto.response.ResponseGetDiscoverMarathon
 import com.runnect.runnect.data.dto.response.ResponseGetDiscoverRecommend
 import com.runnect.runnect.data.dto.response.ResponseGetDiscoverSearch
+import com.runnect.runnect.data.dto.response.ResponseGetDiscoverUploadCourse
 import com.runnect.runnect.data.dto.response.ResponseGetMyDrawCourse
 import com.runnect.runnect.data.dto.response.ResponseGetMyDrawDetail
 import com.runnect.runnect.data.dto.response.ResponseGetMyScrapCourse
