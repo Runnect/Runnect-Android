40ed534a3c629723dd0f82792d8768bf5af9bddd
Donghyeon0915 <dongdong99@kakao.com>
2024-03-22 14:45:18 +0900

    [del] : 테스트 코드 원복

--- app/src/main/java/com/runnect/runnect/data/service/CourseV2Service.kt
+++ app/src/main/java/com/runnect/runnect/data/service/CourseV2Service.kt
@@ -6,6 +6,6 @@ import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 
 interface CourseV2Service {
-    @GET("/api/public-course/ma12rathon")
+    @GET("/api/public-course/marathon")
     fun getMarathonCourse(): Flow<ApiResult<ResponseGetDiscoverMarathon>>
 }
