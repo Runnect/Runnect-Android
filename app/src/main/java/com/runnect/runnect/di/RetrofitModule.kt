48f09388d06cf6a633acce75883377f3d2953944
Donghyeon0915 <dongdong99@kakao.com>
2024-03-22 14:44:19 +0900

    [feat] : V2 Retrofit, OkHttpClient 추가

--- app/src/main/java/com/runnect/runnect/di/RetrofitModule.kt
+++ app/src/main/java/com/runnect/runnect/di/RetrofitModule.kt
@@ -3,7 +3,8 @@ package com.runnect.runnect.di
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.runnect.runnect.BuildConfig
 import com.runnect.runnect.application.ApplicationClass
-import com.runnect.runnect.application.PreferenceManager
+import com.runnect.runnect.data.calladapter.flow.FlowCallAdapterFactory
+import com.runnect.runnect.data.interceptor.ResponseInterceptor
 import com.runnect.runnect.data.service.*
 import com.runnect.runnect.data.repository.*
 import com.runnect.runnect.data.source.remote.*
@@ -20,6 +21,7 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
@@ -32,16 +34,47 @@ object RetrofitModule {
 
     @Qualifier
     @Retention(AnnotationRetention.BINARY)
+    annotation class RetrofitV2
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
     annotation class Tmap
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class HttpClient
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class HttpClientV2
 
     @Provides
     @Singleton
+    @HttpClient
     fun provideOkHttpClient(
         logger: HttpLoggingInterceptor,
         appInterceptor: AppInterceptor,
         tokenAuthenticator: TokenAuthenticator
-    ): OkHttpClient = OkHttpClient.Builder().addInterceptor(logger).addInterceptor(appInterceptor)
-        .authenticator(tokenAuthenticator).build()
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(logger)
+        .addInterceptor(appInterceptor)
+        .authenticator(tokenAuthenticator)
+        .build()
+
+    @Provides
+    @Singleton
+    @HttpClientV2
+    fun provideOkHttpClientV2(
+        logger: HttpLoggingInterceptor,
+        appInterceptor: AppInterceptor,
+        responseInterceptor: ResponseInterceptor,
+        tokenAuthenticator: TokenAuthenticator
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(logger)
+        .addInterceptor(appInterceptor)
+        .addInterceptor(responseInterceptor)
+        .authenticator(tokenAuthenticator)
+        .build()
 
     @Provides
     @Singleton
@@ -55,6 +88,10 @@ object RetrofitModule {
 
     @Provides
     @Singleton
+    fun provideResponseInterceptor(): ResponseInterceptor = ResponseInterceptor()
+
+    @Provides
+    @Singleton
     fun provideTokenAuthenticator(): TokenAuthenticator =
         TokenAuthenticator(ApplicationClass.appContext)
 
@@ -62,12 +99,16 @@ object RetrofitModule {
     @Provides
     @Singleton
     @Runnect
-    fun provideRunnectRetrofit(json: Json, client: OkHttpClient): Retrofit {
+    fun provideRunnectRetrofit(json: Json, @HttpClient client: OkHttpClient): Retrofit {
         kotlinx.coroutines.internal.synchronized(this) {
             val baseUrl = ApplicationClass.getBaseUrl()
-            val retrofit = Retrofit.Builder().baseUrl(baseUrl).client(client)
-                .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-                .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(client)
+                .addConverterFactory(
+                    json.asConverterFactory("application/json".toMediaType())
+                ).build()
+
             return retrofit ?: throw RuntimeException("Retrofit creation failed.")
         }
     }
@@ -75,8 +116,26 @@ object RetrofitModule {
     @OptIn(ExperimentalSerializationApi::class, InternalCoroutinesApi::class)
     @Provides
     @Singleton
+    @RetrofitV2
+    fun provideRunnectRetrofitV2(
+        @HttpClientV2 client: OkHttpClient
+    ): Retrofit {
+        val baseUrl = ApplicationClass.getBaseUrl()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(FlowCallAdapterFactory.create())
+            .build()
+
+        return retrofit ?: throw RuntimeException("Retrofit creation failed.")
+    }
+
+    @OptIn(ExperimentalSerializationApi::class, InternalCoroutinesApi::class)
+    @Provides
+    @Singleton
     @Tmap
-    fun provideTmapRetrofit(json: Json, client: OkHttpClient): Retrofit {
+    fun provideTmapRetrofit(json: Json, @HttpClient client: OkHttpClient): Retrofit {
         kotlinx.coroutines.internal.synchronized(this) {
             val retrofit = Retrofit.Builder().baseUrl(BuildConfig.TMAP_BASE_URL).client(client)
                 .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
