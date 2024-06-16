f5f61290ea33620cd6f8efe29da1fe3874c7d8b4
Donghyeon0915 <dongdong99@kakao.com>
2024-06-16 23:49:37 +0900

    [refactor] MyPage 코드 리팩토링 (Livedata -> StateFlow, SharedFlow)

--- app/src/main/java/com/runnect/runnect/presentation/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/runnect/runnect/presentation/mypage/MyPageViewModel.kt
@@ -1,14 +1,16 @@
 package com.runnect.runnect.presentation.mypage
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import com.runnect.runnect.R
 import com.runnect.runnect.domain.common.toLog
 import com.runnect.runnect.domain.repository.UserRepository
 import com.runnect.runnect.presentation.base.BaseViewModel
-import com.runnect.runnect.presentation.state.UiState
+import com.runnect.runnect.presentation.mypage.dto.UserDto
 import com.runnect.runnect.util.extension.collectResult
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
@@ -17,50 +19,39 @@ class MyPageViewModel @Inject constructor(
     private val userRepository: UserRepository
 ) : BaseViewModel() {
 
-    val nickName: MutableLiveData<String> = MutableLiveData<String>()
-    val stampId: MutableLiveData<String> = MutableLiveData<String>(STAMP_LOCK)
-    val profileImgResId: MutableLiveData<Int> = MutableLiveData<Int>(R.drawable.user_profile_basic)
-    val level: MutableLiveData<String> = MutableLiveData<String>()
-    val levelPercent: MutableLiveData<Int> = MutableLiveData<Int>()
-    val email: MutableLiveData<String> = MutableLiveData<String>()
-
-    private val _userInfoState = MutableLiveData<UiState>(UiState.Loading)
-    val userInfoState: LiveData<UiState>
-        get() = _userInfoState
-
-    val errorMessage = MutableLiveData<String>()
-    fun setNickName(nickName: String) {
-        this.nickName.value = nickName
+    sealed interface UserState {
+        object Loading : UserState
+        data class Success(val user: UserDto) : UserState
+        data class Failure(val message: String) : UserState
     }
 
-    fun setProfileImg(profileImgResId: Int) {
-        this.profileImgResId.value = profileImgResId
+    private val _userState: MutableStateFlow<UserState> = MutableStateFlow(UserState.Loading)
+    val userState: StateFlow<UserState> = _userState.asStateFlow()
+
+    private val _userData = MutableStateFlow(UserDto())
+    val userData: StateFlow<UserDto> = _userData.asStateFlow()
+
+
+    fun updateUser(user: UserDto) {
+        _userState.value = UserState.Success(user).also {
+            _userData.value = user
+        }
     }
 
     fun getUserInfo() = launchWithHandler {
         userRepository.getUserInfo()
+            .flowOn(Dispatchers.IO)
             .onStart {
-                _userInfoState.value = UiState.Loading
-            }.collectResult(
+                _userState.tryEmit(UserState.Loading)
+            }.collectResult (
                 onSuccess = { user ->
-                    user.let {
-                        level.value = it.level.toString()
-                        nickName.value = it.nickname
-                        stampId.value = it.latestStamp
-                        levelPercent.value = it.levelPercent
-                        email.value = it.email
-                    }
-
-                    _userInfoState.value = UiState.Success
+                    val userDto = UserDto.toDto(user)
+                    updateUser(userDto)
                 },
                 onFailure = {
-                    errorMessage.value = it.toLog()
-                    _userInfoState.value = UiState.Failure
+                    _userState.value = UserState.Failure(it.toLog())
+                    sendEvent(EventState.ShowSnackBar(it.toLog()))
                 }
             )
-    }
-
-    companion object {
-        const val STAMP_LOCK = "lock"
     }
 }
