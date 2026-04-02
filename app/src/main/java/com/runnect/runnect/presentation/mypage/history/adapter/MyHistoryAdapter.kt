33569bfa7c154b11b20b7eae79e8e68fc9737207
unam98 <unam98@users.noreply.github.com>
2026-04-02 23:55:56 +0900

    코드 리뷰 피드백 반영
    - compose-bom 2026.03.00 → 2026.03.01 (최신 패치)
    - kakao-story 미사용 의존성 제거 (혼합 버전 해소)
    - Glide sizeMultiplier → thumbnail(RequestBuilder) 패턴으로 변경
      (progressive loading 동작 유지)

--- app/src/main/java/com/runnect/runnect/presentation/mypage/history/adapter/MyHistoryAdapter.kt
+++ app/src/main/java/com/runnect/runnect/presentation/mypage/history/adapter/MyHistoryAdapter.kt
@@ -50,7 +50,8 @@ class MyHistoryAdapter(
         RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: HistoryInfoDTO) {
             with(binding) {
-                Glide.with(itemView).load(data.img).sizeMultiplier(0.3f)
+                Glide.with(itemView).load(data.img)
+                    .thumbnail(Glide.with(itemView).load(data.img).sizeMultiplier(0.3f))
                     .format(DecodeFormat.PREFER_RGB_565)
                     .into(ivMyPageHistoryCourse)
 
